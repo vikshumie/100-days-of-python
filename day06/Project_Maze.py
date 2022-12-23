e45c6454dfd1d15d02bd2d2039d177a3a9d40dc1
Viktoriya <115984905+vikshumie@users.noreply.github.com>
2022-12-23 16:38:06 +0100

    Complete day 6

---
+++ day06/Project_Maze.py
@@ -0,0 +1,12 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+        if right_is_clear():
+            turn_right()
+    else:
+        turn_left()
