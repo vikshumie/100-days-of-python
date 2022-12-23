b756ad489ec3277a13180850f94bf879460b2e50
Viktoriya <115984905+vikshumie@users.noreply.github.com>
2022-12-23 12:00:54 +0100

    Add files via upload

---
+++ day06/Hurdles_exercise3.py
@@ -0,0 +1,13 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+        if right_is_clear():
+            turn_right()
+    else:
+        turn_left()
+        
